31c27195c04422f804a00e5d964352a7533c7e15
cow16g <imamura.kazuki@gmail.com>
2019-06-20 20:02:26 +0900

    [add] 1件更新
    更新処理の追加

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -68,9 +68,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
+        return view('posts.edit', ['post' =>$post]);
     }
 
     /**
@@ -80,9 +80,12 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $post->title = $request->title;
+        $post->body  = $request->body;
+        $post->save();
+        return redirect('posts/'. $post->id);
     }
 
     /**
